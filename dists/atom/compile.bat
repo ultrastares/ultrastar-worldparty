897155416b3aeef53ced3c10b1a0bfd186b6f6fc
TeLiXj <telixj@gmail.com>
2018-01-13 10:20:13 +0100

    add option to only execute the program

--- dists/atom/compile.bat
+++ dists/atom/compile.bat
@@ -1,10 +1,15 @@
 @echo off
 cd %1\src
-rm -rf units\*.*o units\*.a units\*.ppu units\*.rsj
-fpc WorldParty.dpr -FE../game -FUunits
+if not %2=="execute" (
+    rm -rf units\*.*o units\*.a units\*.ppu units\*.rsj
+    fpc WorldParty.dpr -FE../game -FUunits
+)
 if exist "units\link.res" (
     mv units\link.res ..\res\
 )
+if %2=="compile-execute" (
+    start "" ../game/WorldParty -Benchmark
+)
 if %2=="execute" (
-    start ../game/WorldParty
+    start "" ../game/WorldParty -Benchmark
 )
