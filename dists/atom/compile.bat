74921dfeec44799428b1c5273a227da19011f7b8
TeLiXj <telixj@gmail.com>
2018-01-13 11:15:24 +0100

    don't execute program if compilation fails

--- dists/atom/compile.bat
+++ dists/atom/compile.bat
@@ -1,15 +1,17 @@
 @echo off
 cd %1\src
 if not %2=="execute" (
-    rm -rf units\*.*o units\*.a units\*.ppu units\*.rsj
+    rm -rf ..\game\WorldParty* units\*.*o units\*.a units\*.ppu units\*.rsj
     fpc WorldParty.dpr -FE../game -FUunits
 )
 if exist "units\link.res" (
     mv units\link.res ..\res\
 )
-if %2=="compile-execute" (
-    start "" ../game/WorldParty -Benchmark
-)
-if %2=="execute" (
-    start "" ../game/WorldParty -Benchmark
+if exist "..\game\WorldParty.exe" (
+    if %2=="compile-execute" (
+        start "" ../game/WorldParty -Benchmark
+    )
+    if %2=="execute" (
+        start "" ../game/WorldParty -Benchmark
+    )
 )
