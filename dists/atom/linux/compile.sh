fac7ca15e0621599ef756b45c75f0d75f32888d8
TeLiXj <telixj@gmail.com>
2018-01-29 04:24:49 +0100

    add compile debug options

--- dists/atom/linux/compile.sh
+++ dists/atom/linux/compile.sh
@@ -5,18 +5,25 @@ then
     processor=$(uname -m)
     platform=$(uname -s)
     target=../build/fpc-$processor-${platform,,}/
+    rm -rf ../game/WorldParty/* $target
     mkdir -p $target
-    rm -rf ../game/WorldParty* $target/*.*o $target/*.a $target/*.ppu $target/*.rsj
-    fpc WorldParty.dpr -FE../game -FU$target -O4 -CfSSE3
-fi
-if [ -f $target/link.res ]
-then
-    mv $target/link.res ../res/
-fi
-if [ -f ../game/WorldParty ]
-then
-    if [ $2 == "compile-execute" ] || [ $2 == "execute" ]
+    if [ $2 == "compile-debug" ] || [ $2 == "compile-debug-execute" ]
     then
+        fpc WorldParty.dpr -FE../game -FU$target -g -gh -gl -dDEBUG_MODE -Si -Sg -Sc -v0Binwe
+    else
+        fpc WorldParty.dpr -FE../game -FU$target -O4 -Xs
+    fi
+    if [ -f $target/link.res ]
+    then
+        mv $target/link.res ../res/
+    fi
+fi
+if [ -f ../game/WorldParty ] && [ $2 != "compile-debug" ] && [ $2 != "compile" ]
+then
+    if [ $2 == "compile-debug-execute"]
+    then
+        ../game/WorldParty -Benchmark -Debug
+    else
         ../game/WorldParty -Benchmark
     fi
 fi
