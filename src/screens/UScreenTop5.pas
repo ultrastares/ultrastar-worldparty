6c084710f9fcd0cb3b93dc4b6bb9e8118cd43a49
TeLiXj <telixj@gmail.com>
2019-11-26 21:37:14 +0100

    Added back click with right mouse button

--- src/screens/UScreenTop5.pas
+++ src/screens/UScreenTop5.pas
@@ -142,7 +142,7 @@ function TScreenTop5.ParseMouse(MouseButton: integer;
 				X, Y: integer): boolean;
 begin
   Result := true;
-  if (MouseButton = SDL_BUTTON_LEFT) and BtnDown then
+  if ((MouseButton = SDL_BUTTON_LEFT) or (MouseButton = SDL_BUTTON_RIGHT)) and BtnDown then
     //left-click anywhere sends return
     ParseInput(SDLK_RETURN, 0, true);
 end;
