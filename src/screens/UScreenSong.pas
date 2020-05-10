ultrastares/ultrastar-worldparty

commit 4a1ac7e4a7aa58336632dbdb95b6b0327796c0f7

Added song list refresh pressing F5 in main screen and song screen

src/screens/UScreenSong.pas
@@ -582,6 +582,11 @@ begin
             Self.SetJoker();
           end;
         end;
+      SDLK_F5:
+        begin
+          Self.FadeTo(@UGraphic.ScreenMain);
+          UGraphic.ScreenMain.ReloadSongs();
+        end;
     end;
   end;
 end;
@@ -1491,6 +1496,8 @@ var
   I, J:       integer;
   Increment: real;
 begin
+  if USongs.CatSongs.GetVisibleSongs() = 0 then
+    Exit();
 
   FadeMessage();
 

src/base/UIni.pas
@@ -703,21 +703,16 @@ end;
 procedure TIni.LoadPaths(IniFile: TCustomIniFile);
 var
   PathStrings: TStringList;
-  I:           integer;
+  I: integer;
 begin
-  PathStrings := TStringList.Create;
+  UPathUtils.InitializeSongPaths();
+  PathStrings := TStringList.Create();
   IniFile.ReadSection('Directories', PathStrings);
-
-  // Load song-paths
-  for I := 0 to PathStrings.Count-1 do
-  begin
+  for I := 0 to PathStrings.Count - 1 do
     if (Pos('SONGDIR', UpperCase(PathStrings[I])) = 1) then
-    begin
-      AddSongPath(Path(IniFile.ReadString('Directories', PathStrings[I], '')));
-    end;
-  end;
+      UPathUtils.AddSongPath(UPath.Path(IniFile.ReadString('Directories', PathStrings[I], '')));
 
-  PathStrings.Free;
+  PathStrings.Free();
 end;
 
 procedure TIni.LoadThemes(IniFile: TCustomIniFile);

src/base/UPathUtils.pas
@@ -1,8 +1,8 @@
 {*
     UltraStar WorldParty - Karaoke Game
-	
-	UltraStar WorldParty is the legal property of its developers, 
-	whose names	are too numerous to list here. Please refer to the 
+
+	UltraStar WorldParty is the legal property of its developers,
+	whose names	are too numerous to list here. Please refer to the
 	COPYRIGHT file distributed with this source distribution.
 
     This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program. Check "LICENSE" file. If not, see 
+    along with this program. Check "LICENSE" file. If not, see
 	<http://www.gnu.org/licenses/>.
  *}
 
@@ -57,7 +57,8 @@ var
   AvatarsPath:      IPath;
 
 function FindPath(out PathResult: IPath; const RequestedPath: IPath; NeedsWritePermission: boolean): boolean;
-procedure InitializePaths;
+procedure InitializePaths();
+procedure InitializeSongPaths();
 procedure AddSongPath(const Path: IPath);
 
 implementation
@@ -187,19 +188,21 @@ begin
     Log.LogWarn('Screenshot directory "'+ UserPath.ToNative +'" not available', 'InitializePaths');
   end;
 
-  // Add song paths
-  AddSongPath(Params.SongPath);
-{$IF Defined(DARWIN)}
-  AddSongPath(Platform.GetMusicPath);
-  AddSongPath(UserPath.Append('songs'));
-{$ELSE}
-  AddSongPath(SharedPath.Append('songs'));
-  AddSongPath(UserPath.Append('songs'));
-{$IFEND}
-
   // Add category cover paths
   AddCoverPath(SharedPath.Append('covers'));
   AddCoverPath(UserPath.Append('covers'));
+end;
+
+procedure InitializeSongPaths();
+begin
+  SongPaths := TInterfaceList.Create();
+  AddSongPath(UCommandLine.Params.SongPath);
+  AddSongPath(UPlatform.Platform().GetGameUserPath().Append('songs'));
+  {$IF Defined(DARWIN)}
+    AddSongPath(UPlatform.Platform().GetMusicPath());
+  {$ELSE}
+    AddSongPath(UPlatform.Platform().GetGameSharedPath().Append('songs'));
+  {$IFEND}
 end;
 
 end.

src/screens/UScreenMain.pas
@@ -49,9 +49,11 @@ type
       function ParseInput(PressedKey: Cardinal; CharCode: UCS4Char; PressedDown: boolean): boolean; override;
       function Draw: boolean; override;
       procedure OnShow; override;
+      procedure ReloadSongs(ScreenReturn: boolean = true);
       procedure SetInteraction(Num: integer); override;
       procedure SetAnimationProgress(Progress: real); override;
     private
+      ReturnToSongScreenAfterLoadSongs: boolean;
       TextDescription, TextDescriptionLong, TextProgressSongs: integer;
       function CheckSongs(): boolean;
   end;
@@ -106,7 +108,7 @@ begin
           case Interaction of
             0: //solo
             begin
-              if Self.CheckSongs then
+              if Self.CheckSongs() then
               begin
                 UGraphic.ScreenSong.Mode := smNormal;
                 if (Ini.Players >= 0) and (Ini.Players <= 3) then
@@ -128,7 +130,7 @@ begin
             end;
             1: //party
             begin
-              if Self.CheckSongs then
+              if Self.CheckSongs() then
               begin
                 if not Assigned(UGraphic.ScreenPartyOptions) then //load the screens only the first time
                   UGraphic.ScreenPartyOptions := TScreenPartyOptions.Create();
@@ -142,7 +144,7 @@ begin
               if not Assigned(UGraphic.ScreenJukeboxPlaylist) then //load the screens only the first time
                 UGraphic.ScreenJukeboxPlaylist := TScreenJukeboxPlaylist.Create();
 
-              if Self.CheckSongs then
+              if Self.CheckSongs() then
                 FadeTo(@ScreenJukeboxPlaylist, SoundLib.Start);
             end;
             3: //stats
@@ -150,7 +152,7 @@ begin
               if not Assigned(UGraphic.ScreenStatMain) then //load the screens only the first time
                 UGraphic.ScreenStatMain := TScreenStatMain.Create();
 
-              if Self.CheckSongs then
+              if Self.CheckSongs() then
                 FadeTo(@ScreenStatMain, SoundLib.Start);
             end;
             4: //options
@@ -179,6 +181,8 @@ begin
         InteractNext;
       SDLK_LEFT:
         InteractPrev;
+      SDLK_F5:
+        Self.ReloadSongs();
     end;
   end
 end;
@@ -210,6 +214,7 @@ begin
 
   AddButton(Theme.Main.ButtonAbout);
   Interaction := 0;
+  Self.ReturnToSongScreenAfterLoadSongs := false;
 end;
 
 function TScreenMain.Draw: boolean;
@@ -220,15 +225,22 @@ begin
   Result := true;
   ProgressSong := USongs.Songs.GetLoadProgress();
   if not ProgressSong.Finished then //while song loading show progress
+    Self.Text[Self.TextProgressSongs].Text := ProgressSong.Folder+': '+IntToStr(ProgressSong.Total)+'\n\n'+ProgressSong.FolderProcessed
+  else if not Self.Text[Self.TextDescriptionLong].Visible then //after finish song loading, return to normal mode and close popup
   begin
-    Self.Text[TextDescriptionLong].Visible := false;
-    Self.Text[TextProgressSongs].Text := ProgressSong.Folder+': '+IntToStr(ProgressSong.Total)+'\n\n'+ProgressSong.FolderProcessed;
-  end
-  else if Self.Text[TextDescriptionLong].Visible = false then //after finish song loading, return to normal mode and close popup
-  begin
+    FreeAndNil(UGraphic.ScreenSong);
     UGraphic.ScreenSong := TScreenSong.Create();
-    Self.Text[TextDescriptionLong].Visible := true;
-    Self.Text[TextProgressSongs].Visible := false;
+    if Self.ReturnToSongScreenAfterLoadSongs then
+    begin
+      Self.ReturnToSongScreenAfterLoadSongs := false;
+      if Self.CheckSongs() then
+      begin
+        UDisplay.Display.AbortScreenChange();
+        Self.FadeTo(@UGraphic.ScreenSong);
+      end;
+    end;
+    Self.Text[Self.TextDescriptionLong].Visible := true;
+    Self.Text[Self.TextProgressSongs].Visible := false;
     if ProgressSong.Total > 0 then
       UGraphic.ScreenPopupError.Visible := false;
   end;
@@ -237,7 +249,8 @@ end;
 procedure TScreenMain.OnShow;
 begin
   inherited;
-
+  Self.Text[Self.TextProgressSongs].Visible := true;
+  Self.Text[Self.TextDescriptionLong].Visible := false;
   SoundLib.StartBgMusic;
 
  {**
@@ -270,6 +283,16 @@ begin
     UGraphic.ScreenPopupError.ShowPopup(Language.Translate('ERROR_NO_SONGS'))
   else
     Result := true;
+end;
+
+procedure TScreenMain.ReloadSongs(ScreenReturn: boolean = true);
+begin
+  UIni.Ini.Load();
+  FreeAndNil(USongs.CatSongs);
+  FreeAndNil(USongs.Songs);
+  USongs.CatSongs := TCatSongs.Create();
+  USongs.Songs := TSongs.Create();
+  Self.ReturnToSongScreenAfterLoadSongs := ScreenReturn;
 end;
 
 end.

src/screens/UScreenOptionsGame.pas
@@ -96,9 +96,7 @@ begin
             if Self.SelInteraction = 6 then //rebuild songs arrays with new config
             begin
               UIni.Ini.Save();
-              FreeAndNil(UGraphic.ScreenSong);
-              USongs.CatSongs := TCatSongs.Create();
-              USongs.Songs := TSongs.Create();
+              UGraphic.ScreenMain.ReloadSongs(false);
             end;
 
             AudioPlayback.PlaySound(SoundLib.Option);

commit 8102a496af02c041cfd6007fb1debbb6ccd7d0b9

Fixed song search that was broken in 4a1ac7e4a7aa58336632dbdb95b6b0327796c0f7

src/screens/UScreenSong.pas
@@ -1538,11 +1538,9 @@ var
   I, J:       integer;
   Increment: real;
 begin
-  if USongs.CatSongs.GetVisibleSongs() = 0 then
-  begin
-    Result := false;
+  Result := true;
+  if (USongs.CatSongs.GetVisibleSongs() = 0) and (not UGraphic.ScreenSongJumpTo.Visible) then
     Exit();
-  end;
 
   FadeMessage();
 
@@ -1724,8 +1722,6 @@ begin
 
   //if (Mode = smPartyTournament) then
   //  PartyTimeLimit();
-
-  Result := true;
 end;
 
 procedure TScreenSong.StartPreview();