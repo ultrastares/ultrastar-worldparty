62c4c139c3f23bf02989972d547dbf91eef4c5c2
Daniel20 <d.noguero@hotmail.com>
2020-05-15 22:58:59 +0200

    New song icons & update translations

--- src/base/UIni.pas
+++ src/base/UIni.pas
@@ -352,7 +352,7 @@ const
   ILyricsAlpha: array[0..19] of UTF8String = ('0.05', '0.10', '0.15', '0.20', '0.25', '0.30', '0.35', '0.40', '0.45', '0.50', '0.55', '0.60', '0.65', '0.70', '0.75', '0.80', '0.85', '0.90', '0.95', '1.00');
 
   //for lyric colors
-  ILine:             array[0..2] of UTF8String = ('Sing', 'Actual', 'Next');
+  ILine:             array[0..2] of UTF8String = ('Sing', 'Top', 'Bottom');
   IProperty: array[0..1] of UTF8String = ('Fill', 'Outline');
   LineColor: array[0..21] of UTF8String = ('Blue', 'Green', 'Pink', 'Red', 'Violet', 'Orange', 'Yellow', 'Magenta', 'Brown', 'Black', 'Turquoise', 'Salmon', 'GreenYellow', 'Lavender', 'Beige', 'Teal', 'Orchid', 'SteelBlue', 'Plum', 'Chocolate', 'Gold', 'Other');
   LineInactiveColor: array[0..9] of UTF8String = ('Black', 'Gray +3', 'Gray +2', 'Gray +1', 'Gray', 'Gray -1', 'Gray -2', 'Gray -3', 'White', 'Other');
