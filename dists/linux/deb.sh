b192e232dadfd190a20623b9b4c872b536608ab6
TeLiXj <telixj@gmail.com>
2021-02-07 17:25:11 +0100

    Added full automatic build

--- dists/linux/deb.sh
+++ dists/linux/deb.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-if [ $# != 3 ]
-then
-    echo "Version and size not provided."
-    echo "You must add 3 parameters: main_version specific_version size_in_bytes"
-    echo "for example: 20.12 1 93396"
-    exit
-fi
-version=ultrastar-worldparty_$1-$2_amd64
+version_numeric=$(head -n 1 ../../VERSION)-${1:-1}
+version=ultrastar-worldparty_${version_numeric}_amd64
 build=dists/linux/$version
 build_game=../$build
 mkdir $version
@@ -14,19 +8,22 @@ cd $version
 mkdir -p usr/games usr/share/ultrastar-worldparty usr/share/applications usr/share/icons/hicolor/256x256/apps
 cd ..
 cp ultrastar-worldparty.desktop $version/usr/share/applications/ultrastar-worldparty.desktop
-cd ../../game
+cd ../atom/linux
+./compile.sh ../../.. compile-snap
+./compile.sh ../../.. compile-snap-debug
+cd ../../../game
 cp WorldParty $build_game/usr/games/ultrastar-worldparty
 cp WorldPartyDebug $build_game/usr/games/ultrastar-worldparty-debug
 cp resources/icons/WorldParty.png $build_game/usr/share/icons/hicolor/256x256/apps/ultrastar-worldparty.png
-cp -r avatars covers fonts languages plugins resources sounds themes visuals $build_game/usr/share/ultrastar-worldparty
+cp -r avatars covers fonts languages licenses plugins resources sounds themes $build_game/usr/share/ultrastar-worldparty
 cd $build_game
-mkdir DEBIAN
+mkdir DEBIAN usr/share/ultrastar-worldparty/songs usr/share/ultrastar-worldparty/playlists
 echo "Package: ultrastar-worldparty
-Version: 20.12-1
+Version: $version_numeric
 Section: games
 Priority: optional
 Architecture: amd64
-Installed-Size: $3
+Installed-Size: $(du -s usr | cut -f1)
 Depends: libsdl2-image-dev, libavformat-dev, libswscale-dev, libsqlite3-dev, libfreetype6-dev, libportmidi-dev, liblua5.3-dev, libopencv-highgui-dev
 Maintainer: TeLiXj <telixj@gmail.com>
 Homepage: https://ultrastar-es.org
